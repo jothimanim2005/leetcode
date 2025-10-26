18d7567eefa57da5f576759979a5f88124a70b3e
jothimanim2005 <jothimanim3245@gmail.com>
2025-10-26 15:35:55 +0530

    Time: 3 ms (65.21%) | Memory: 45.3 MB (82.12%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            int mask = 1 << i;
+            for (int num : nums) {
+                if ((num & mask) != 0) {
+                    sum++;
+                }
+            }
+            if (sum % 3 != 0) {
+                result |= mask;
+            }
+        }
+
+        return result;
+        
+    }
+}
