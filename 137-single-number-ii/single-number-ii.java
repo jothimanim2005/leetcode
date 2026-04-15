4bf5d9ed7c78d8d5213ea2d4746187f110c1c89f
jothimanim2005 <jothimanim3245@gmail.com>
2026-04-15 17:09:51 +0530

    Time: 7 ms (21.11%) | Memory: 46 MB (28.66%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,20 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-         int result = 0;
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-            int mask = 1 << i;
-            for (int num : nums) {
-                if ((num & mask) != 0) {
-                    sum++;
-                }
-            }
-            if (sum % 3 != 0) {
-                result |= mask;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=3){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
             }
         }
-
-        return result;
-        
+        return nums[nums.length-1];
     }
 }
